51e286aa294db8089e5f1be1e623adc8460f5ed3
UKnow-IKnow <sarojsahu014@gmail.com>
2022-03-05 11:13:02 +0530

    setup Database

---
+++ app/src/main/java/com/example/runningtracker/data/Database.kt
@@ -0,0 +1,18 @@
+package com.example.runningtracker.data
+
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [Run::class],
+    version = 1
+)
+@TypeConverters(
+    Converters::class
+)
+abstract class Database: RoomDatabase() {
+
+    abstract fun getRunDao(): RunDAO
+}

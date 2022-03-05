cba9bd7f50b7eb9ecb0f36e9aec5a421e1ee2156
UKnow-IKnow <sarojsahu014@gmail.com>
2022-03-05 17:47:34 +0530

    setup bottom navigation menu and navigate properly

--- app/src/main/java/com/example/runningtracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/runningtracker/ui/MainActivity.kt
@@ -3,20 +3,36 @@ package com.example.runningtracker.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.runningtracker.R
 import com.example.runningtracker.data.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDAO: RunDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("runDao","RUNDAO: ${runDAO.hashCode()}")
+
+
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        //don't show bottom navigation view in tracking and welcome fragment
+        //for that we use on destinationchange listener
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 }

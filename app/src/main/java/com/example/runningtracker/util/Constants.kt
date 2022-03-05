c2c8cce5650a2fb87a052a4c8d5e169348319017
UKnow-IKnow <sarojsahu014@gmail.com>
2022-03-05 23:17:28 +0530

    Handled Location permission

--- app/src/main/java/com/example/runningtracker/util/Constants.kt
+++ app/src/main/java/com/example/runningtracker/util/Constants.kt
@@ -3,4 +3,6 @@ package com.example.runningtracker.util
 object Constants {
     const val DATABASE_TABLE = "run_table"
     const val DATABASE_NAME = "run_database"
+
+    const val REQUEST_CODE_LOCATION_PERMISSION = 0
 }

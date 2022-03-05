89aeef21a259299a05634ca0e350c8a7000988d5
UKnow-IKnow <sarojsahu014@gmail.com>
2022-03-05 14:19:19 +0530

    create all fragment and added viewModel

---
+++ app/src/main/java/com/example/runningtracker/ui/fragments/RunFragment.kt
@@ -0,0 +1,13 @@
+package com.example.runningtracker.ui.fragments
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import com.example.runningtracker.R
+import com.example.runningtracker.ui.viewModels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RunFragment : Fragment(R.layout.fragment_run) {
+    private val viewModel: MainViewModel by viewModels()
+}
---
+++ app/src/main/java/com/example/runningtracker/ui/fragments/WelcomeFragment.kt
@@ -0,0 +1,8 @@
+package com.example.runningtracker.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.runningtracker.R
+
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+
+}

5763771839b7cf3ce88da6f20aedc211b7ebb050
UKnow-IKnow <sarojsahu014@gmail.com>
2022-03-07 00:11:03 +0530

    setup with notification channel

--- app/src/main/java/com/example/runningtracker/services/TrackingService.kt
+++ app/src/main/java/com/example/runningtracker/services/TrackingService.kt
@@ -1,18 +1,28 @@
 package com.example.runningtracker.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import com.bumptech.glide.load.engine.executor.GlideExecutor.UncaughtThrowableStrategy.LOG
+import com.example.runningtracker.R
 import com.example.runningtracker.util.Constants.ACTION_PAUSE_SERVICE
 import com.example.runningtracker.util.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.example.runningtracker.util.Constants.ACTION_STOP_SERVICE
+import com.example.runningtracker.util.Constants.NOTIFICATION_CHANNEL_ID
+import com.example.runningtracker.util.Constants.NOTIFICATION_CHANNEL_NAME
 import timber.log.Timber
 
-class TrackingService: LifecycleService() {
+class TrackingService : LifecycleService() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
-            when(it.action){
+            when (it.action) {
                 ACTION_START_OR_RESUME_SERVICE -> {
                     Timber.d("started or resume service")
                 }
@@ -26,4 +36,29 @@ class TrackingService: LifecycleService() {
         }
         return super.onStartCommand(intent, flags, startId)
     }
+
+    private fun startForegroundService(){
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+                as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            createNotificationChannel(notificationManager)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_direction_run_black)
+            .setContentTitle("Running App")
+            .setContentText("00:00:00")
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channel =
+            NotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME, IMPORTANCE_LOW)
+        notificationManager.createNotificationChannel(channel)
+    }
+
+
 }

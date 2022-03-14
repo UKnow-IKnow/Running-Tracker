98a815c6868d6ca250d907f30b4b2d38d89549b0
UKnow-IKnow <sarojsahu014@gmail.com>
2022-03-14 23:20:23 +0530

    bug find

--- app/src/main/java/com/example/runningtracker/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/runningtracker/ui/fragments/TrackingFragment.kt
@@ -4,10 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.runningtracker.R
+import com.example.runningtracker.data.Run
 import com.example.runningtracker.services.Polyline
 import com.example.runningtracker.services.TrackingService
 import com.example.runningtracker.ui.viewModels.MainViewModel
@@ -23,6 +25,7 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
 import java.util.*
@@ -31,7 +34,7 @@ import kotlin.math.round
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
-    private val viewModel: MainViewModel by viewModels()
+    private val viewModel by activityViewModels<MainViewModel>()
 
     private var isTracking = false
     private var pathPoints = mutableListOf<Polyline>()
@@ -41,6 +44,8 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
     private var currentTimeInMillis = 0L
 
     private var menu: Menu? = null
+
+    private var weight = 80f
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -58,6 +63,12 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         btnToggleRun.setOnClickListener {
             toggleRun()
         }
+
+        btnFinishRun.setOnClickListener {
+            zoomToSeeWholeTrack()
+            endRunAndSaveToDatabase()
+        }
+
         mapView.getMapAsync {
             map = it
             addAllPolyline()
@@ -184,7 +195,15 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
             }
             val avgSpeed = round((distanceInMeter / 1000f) / (currentTimeInMillis / 1000f / 60 / 60) * 10) / 10f
             val dateTimestamp = Calendar.getInstance().timeInMillis
-
+            val caloriesBurned = ((distanceInMeter / 1000f) * weight).toInt()
+            val run = Run(bitmap,dateTimestamp,avgSpeed,distanceInMeter,currentTimeInMillis, caloriesBurned)
+            viewModel.insertRun(run)
+            Snackbar.make(
+                requireActivity().findViewById(R.id.rootView),
+                "Run saved successfully",
+                Snackbar.LENGTH_LONG
+            ).show()
+            stopRun()
         }
     }
 
--- app/src/main/java/com/example/runningtracker/ui/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/runningtracker/ui/viewModels/MainViewModel.kt
@@ -3,10 +3,17 @@ package com.example.runningtracker.ui.viewModels
 import android.view.View
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.runningtracker.data.Run
 import com.example.runningtracker.data.repositories.MainRepository
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
+
+    fun insertRun(run : Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 }

d8d7ee1ef297d9a42a45ce53901da75eb3dbbb7f
UKnow-IKnow <sarojsahu014@gmail.com>
2022-03-17 19:24:03 +0530

    add runs in recycle view

---
+++ app/src/main/java/com/example/runningtracker/adapters/RunAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.runningtracker.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.runningtracker.R
+import com.example.runningtracker.data.Run
+import kotlinx.android.synthetic.main.item_run.view.*
+
+class RunAdapter : RecyclerView.Adapter<RunAdapter.RunViewHolder>() {
+
+    inner class RunViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    //List differ: A tool that takes two list and calculate the difference between them
+    val differCallBack = object : DiffUtil.ItemCallback<Run>(){
+        override fun areItemsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return  oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Run, newItem: Run): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    }
+
+    val differ = AsyncListDiffer(this,differCallBack)
+
+    fun submitList(list: List<Run>) = differ.submitList(list)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RunViewHolder {
+        return RunViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_run,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RunViewHolder, position: Int) {
+        val run = differ.currentList[position]
+        holder.itemView.apply {
+            Glide.with(this).load(run.img).into(ivRunImage)
+
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+}

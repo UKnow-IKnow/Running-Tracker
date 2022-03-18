a59bf7b3bb2a34d400120d56885e36edb97d324b
UKnow-IKnow <sarojsahu014@gmail.com>
2022-03-18 22:50:13 +0530

    bug fix

--- app/src/main/java/com/example/runningtracker/ui/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/runningtracker/ui/viewModels/MainViewModel.kt
@@ -10,7 +10,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-
+@HiltViewModel
 class MainViewModel @Inject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {

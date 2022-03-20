a3eb0bdfbf3c3915bef494693bc7c37d1aa9932a
UKnow-IKnow <sarojsahu014@gmail.com>
2022-03-20 21:19:01 +0530

    set up with welcome fragment

--- app/src/main/java/com/example/runningtracker/di/DatabaseModule.kt
+++ app/src/main/java/com/example/runningtracker/di/DatabaseModule.kt
@@ -1,10 +1,12 @@
 package com.example.runningtracker.di
 
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.runningtracker.data.Database
 import com.example.runningtracker.util.Constants.DATABASE_NAME
+import com.example.runningtracker.util.Constants.SHARED_PREFERENCE_NAME
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -29,5 +31,11 @@ object DatabaseModule {
     @Singleton
     @Provides
     fun provideDao(database: Database) = database.getRunDao()
+
+    @Singleton
+    @Provides
+    fun provideSharedPreference(
+        @ApplicationContext context: Context
+    ) = context.getSharedPreferences(SHARED_PREFERENCE_NAME, MODE_PRIVATE)
 
 }
--- app/src/main/java/com/example/runningtracker/ui/fragments/WelcomeFragment.kt
+++ app/src/main/java/com/example/runningtracker/ui/fragments/WelcomeFragment.kt
@@ -1,13 +1,20 @@
 package com.example.runningtracker.ui.fragments
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.runningtracker.R
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_welcome.*
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+
+    @Inject
+    lateinit var sharedPreference: SharedPreferences
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -15,6 +22,16 @@ class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
         tvContinue.setOnClickListener {
             findNavController().navigate(R.id.action_welcomeFragment_to_runFragment)
         }
+    }
+
+    private fun writePersonalDataToSharedPreference(): Boolean {
+        val name = etName.text.toString()
+        val weight = etWeight.text.toString()
+        if(name.isEmpty() || weight.isEmpty()){
+            return false
+        }
+        sharedPreference.edit()
+            .putString()
     }
 
 }

9a9177b744b454a4cfe020019a99e68af359089b
UKnow-IKnow <sarojsahu014@gmail.com>
2022-03-24 15:55:50 +0530

    load Fields From SharedPreference

--- app/src/main/java/com/example/runningtracker/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/runningtracker/ui/fragments/SettingsFragment.kt
@@ -6,6 +6,7 @@ import com.example.runningtracker.R
 import com.example.runningtracker.util.Constants.KEY_NAME
 import com.example.runningtracker.util.Constants.KEY_WEIGHT
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_settings.*
 import javax.inject.Inject
 
@@ -16,16 +17,27 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
     @Inject
     lateinit var sharedPreferences: SharedPreferences
 
-    private fun applyChangesToSharedPref(): Boolean {
+    private fun loadFieldsFromSharedPreference(){
+        val name = sharedPreferences.getString(KEY_NAME, "")
+        val weight = sharedPreferences.getFloat(KEY_WEIGHT, 80f)
+        etName.setText(name)
+        etWeight.setText(weight.toString())
+    }
+
+    private fun applyChangesToSharedPreference(): Boolean {
         val nameText = etName.text.toString()
         val weightText = etWeight.text.toString()
-        if (nameText.isEmpty() || weightText.isEmpty()){
+        if (nameText.isEmpty() || weightText.isEmpty()) {
             return false
         }
         sharedPreferences.edit()
-            .putString(KEY_NAME,nameText)
-            .putFloat(KEY_WEIGHT,weightText.toFloat())
+            .putString(KEY_NAME, nameText)
+            .putFloat(KEY_WEIGHT, weightText.toFloat())
             .apply()
+
+        val toolBalText = "Let's go $nameText"
+        requireActivity().tvToolbarTitle.text = toolBalText
+        return true
     }
 
 }

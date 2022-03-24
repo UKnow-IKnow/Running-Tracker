29807af20ebbc4d83c9f9ac602ba19d5686936aa
UKnow-IKnow <sarojsahu014@gmail.com>
2022-03-24 15:46:07 +0530

    added shared preference to setting fragment

--- app/src/main/java/com/example/runningtracker/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/runningtracker/ui/fragments/SettingsFragment.kt
@@ -1,8 +1,31 @@
 package com.example.runningtracker.ui.fragments
 
+import android.content.SharedPreferences
 import androidx.fragment.app.Fragment
 import com.example.runningtracker.R
+import com.example.runningtracker.util.Constants.KEY_NAME
+import com.example.runningtracker.util.Constants.KEY_WEIGHT
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_settings.*
+import javax.inject.Inject
 
+
+@AndroidEntryPoint
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    private fun applyChangesToSharedPref(): Boolean {
+        val nameText = etName.text.toString()
+        val weightText = etWeight.text.toString()
+        if (nameText.isEmpty() || weightText.isEmpty()){
+            return false
+        }
+        sharedPreferences.edit()
+            .putString(KEY_NAME,nameText)
+            .putFloat(KEY_WEIGHT,weightText.toFloat())
+            .apply()
+    }
 
 }

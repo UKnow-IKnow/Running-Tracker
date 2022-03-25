3208d3a1b53240314cbc08ef1e351e1fca215423
UKnow-IKnow <sarojsahu014@gmail.com>
2022-03-25 15:53:55 +0530

    done with setting fragment

--- app/src/main/java/com/example/runningtracker/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/runningtracker/ui/fragments/SettingsFragment.kt
@@ -1,10 +1,13 @@
 package com.example.runningtracker.ui.fragments
 
 import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.runningtracker.R
 import com.example.runningtracker.util.Constants.KEY_NAME
 import com.example.runningtracker.util.Constants.KEY_WEIGHT
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_settings.*
@@ -16,6 +19,20 @@ class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
     @Inject
     lateinit var sharedPreferences: SharedPreferences
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnApplyChanges.setOnClickListener{
+            val success = applyChangesToSharedPreference()
+            if(success){
+                Snackbar.make(view, "Saved changes",Snackbar.LENGTH_LONG).show()
+            }else{
+                Snackbar.make(view,"Please fill out all the fields",Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
 
     private fun loadFieldsFromSharedPreference(){
         val name = sharedPreferences.getString(KEY_NAME, "")

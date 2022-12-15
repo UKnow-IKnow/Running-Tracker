6a38e9523bebcd002ffd434d866ea95e6ffa9354
UKnow-IKnow <sarojsahu014@gmail.com>
2022-12-15 14:29:11 +0530

    dependencies upgrade

--- app/src/main/java/com/example/runningtracker/ui/MainActivity.kt
+++ app/src/main/java/com/example/runningtracker/ui/MainActivity.kt
@@ -17,7 +17,6 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
